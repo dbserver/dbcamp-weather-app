082751df5a1adc6ac4b059edc48065dbb21edb29
mateus.santana <mateus.santana@dbserver.com.br>
2023-04-28 18:04:31 -0300

    feat/ add buttons

--- src/pages/cadastrar/index.tsx
+++ src/pages/cadastrar/index.tsx
@@ -1,6 +1,30 @@
 import styles from "./cadastrar.module.css";
+import axios from "axios";
 
 export default function Cadastrar() {
+  async function post() {
+    axios
+      .post("http://localhost:4767/api/v1/meteorologia", {
+        cidade: "Madre de deus",
+        data: "2023-04-28",
+        tempo: "SOL",
+        turno: "DIA",
+        temperaturaMaxima: 0,
+        temperaturaMinima: 0,
+        precipitacao: 0,
+        umidade: 0,
+        velocidadeVentos: 0,
+      })
+      .then(function (response:any) {
+        console.log(response);
+        alert("Parabéns você cadastrou um registro");
+      })
+      .catch(function (error:any) {
+        console.error(error);
+        alert("Infelizmente você não conseguiu cadastrar um registro");
+      });
+  }
+
   return (
     <div className={styles.conteiner}>
       <h1>Cadastro Meteorológico</h1>
@@ -19,13 +43,13 @@ export default function Cadastrar() {
         </div>
 
         <div className={styles.form_box}>
-
           <div className={styles.coluna}>
-
             <div className={styles.content_input} id={styles.tempoBox}>
               <label htmlFor="">Tempo</label>
               <select name="select" className={styles.inputBox}>
-                <option value="SOL" selected>Sol</option>
+                <option value="SOL" selected>
+                  Sol
+                </option>
                 <option value="CHUVA">Chuva</option>
                 <option value="NUBLADO">Nublado</option>
                 <option value="TEMPESTADE">Tempestade</option>
@@ -36,7 +60,9 @@ export default function Cadastrar() {
             <div className={styles.content_input}>
               <label htmlFor="">Turno</label>
               <select name="select">
-                <option value="DIA" selected>Manhã</option>
+                <option value="DIA" selected>
+                  Manhã
+                </option>
                 <option value="NOITE">Noite</option>
               </select>
             </div>
@@ -57,8 +83,11 @@ export default function Cadastrar() {
             <label htmlFor="">Velocidade do vento</label>
             <input type="number" name="velocidadeDoVento" id="" />
           </div>
-          
         </div>
+      </div>
+      <div className={styles.buttonsBox}>
+        <button>Cancelar</button>
+        <button onClick={post}>Cadastrar</button>
       </div>
     </div>
   );
