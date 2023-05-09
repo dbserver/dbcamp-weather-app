a242a934e36095c4b6fc33f8d4a3686f37330926
mateus.santana <mateus.santana@dbserver.com.br>
2023-05-09 12:30:07 -0300

    Refractor/ evitando erros da home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,8 +3,10 @@ import { MeteorologicalService } from "../../services/api/meteorogical";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [meteorologicalRegisterList, setMeteorologicalRegisterList] = useState([]);
-  
+  const [meteorologicalRegisterList, setMeteorologicalRegisterList] = useState(
+    []
+  );
+
   useEffect(() => {
     const fetchData = async () => {
       const result = await MeteorologicalService.getALL();
@@ -14,15 +16,15 @@ export default function Home() {
   }, []);
 
   return (
-
-    <div>
-      {meteorologicalRegisterList.map((item: MeteriologicalData ) => (
-        <div key={item.id}>
-          <h1>{item.cidade}</h1>
-          <h2>{item.tempo}</h2>
-          <p>{item.turno}</p>
-        </div>
-      ))}
-    </div>
+    <h1>hello home</h1>
+    // <div>
+    //   {meteorologicalRegisterList.map((item: MeteriologicalData ) => (
+    //     <div key={item.id}>
+    //       <h1>{item.cidade}</h1>
+    //       <h2>{item.tempo}</h2>
+    //       <p>{item.turno}</p>
+    //     </div>
+    //   ))}
+    // </div>
   );
 }
